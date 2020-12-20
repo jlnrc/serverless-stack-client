9765e02da802904709fa5d63bb2f0e92ffd50ece
Forster, Julian <Julian.Forster@haufe-lexware.com>
2020-12-20 14:24:21 +0100

    Add more stuff to react app

--- src/Routes.js
+++ src/Routes.js
@@ -1,13 +1,21 @@
-import React from "react";
-import { Route, Switch } from "react-router-dom";
-import Home from "./containers/Home";
-import NotFound from "./containers/NotFound";
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import Home from './containers/Home';
+import Login from './containers/Login';
+import Signup from "./containers/Signup";
+import NotFound from './containers/NotFound';
 
 export default function Routes() {
   return (
     <Switch>
       <Route exact path="/">
         <Home />
+      </Route>
+      <Route exact path="/login">
+        <Login />
+      </Route>
+      <Route exact path="/signup">
+        <Signup />
       </Route>
       {/* Finally, catch all unmatched routes */}
       <Route>
